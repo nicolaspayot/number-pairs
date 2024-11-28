12c5fe71db04c40b4ae1f3a27fe7a757e4cd7c26
Nicolas Payot <nicolas.payot@gmail.com>
2024-11-28 08:47:38 +0100

    chore: add prettier

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+const config = {
+  printWidth: 120,
+  quoteProps: "as-needed",
+  singleQuote: true,
+  tabWidth: 4,
+  trailingComma: "all",
+  vueIndentScriptAndStyle: true,
+};
+
+export default config;
